76a6c32c53c0809337f5660c76aa338a8240f5f0
gcv217 <gcv217@lehigh.edu>
2014-08-29 14:25:47 +0000

    updated HelloWorld

---
+++ Lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////////
+//Guerric Vornle von Haagenfels
+//Lab01
+//Label class
+ public class HelloWorld{
+     //Main method
+     public static void main(String[] args) {
+         //print statement with box around it
+         System.out.println("########################");
+         System.out.println("########################");
+         System.out.println("#                      #");
+         System.out.println("#  Hello World 2014!!  #");
+         System.out.println("#                      #");
+         System.out.println("########################");
+         System.out.println("########################");
+         System.out.println("test");
+     //test/testtest
+          }
+ }
